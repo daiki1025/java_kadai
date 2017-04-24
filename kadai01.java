f6b5ed9648033fdbf65f90ae77e9edb6605bfba3
daiki1025 <pengin1025d@gmail.com>
2017-04-24 17:10:31 +0900

    kadai01
    FizBuzz

---
+++ kadai01.java
@@ -0,0 +1,16 @@
+class kadai01 {
+  public static void  main(String[] args) {
+
+    int i = 1;
+    while(i <= 16) {
+      if(i % 3 == 0) {
+        System.out.println("Fizz");
+      }else if (i % 5 == 0) {
+        System.out.println("Buzz");
+      }else{
+        System.out.println(i);
+      }
+         i++;
+      }
+    }
+}
